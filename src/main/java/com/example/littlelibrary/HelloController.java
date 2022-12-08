ee352ba2c4eaaacb53265d87fec5ce6a0406c91e
haley <haleyd1093@gmail.com>
2022-12-08 11:58:17 -0600

    11/20

--- src/main/java/com/example/littlelibrary/HelloController.java
+++ src/main/java/com/example/littlelibrary/HelloController.java
@@ -4,9 +4,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.net.URL;
 import java.sql.*;
 
 
@@ -110,134 +115,38 @@ public class HelloController {
     private void getQuery() {
     }
 
+    @FXML Button addBtn;
+    @FXML Button trackBtn;
+    @FXML Button summaryBtn;
+    @FXML Button browseBtn;
 
-//
-//    private Dialog<Books> createBookDialog (Books books) {
-//        Dialog<Books> dialog = new Dialog<>();
-//        dialog.setTitle("Add Dialog");
-//        dialog.setHeaderText("Add a new book to the database");
-//        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL);
-//        Stage dialogWindow = (Stage) dialog.getDialogPane().getScene().getWindow();
-//
-//        GridPane grid = new GridPane();
-//        grid.setHgap(10);
-//        grid.setVgap(10);
-//        grid.setPadding(new Insets(20, 150, 10, 10));
-//
-//        TextField title = new TextField();
-//        title.setPromptText("Title");
-//        TextField author = new TextField();
-//        author.setPromptText("Author");
-//        TextField finishDate = new TextField();
-//        finishDate.setPromptText("Finish Date");
-//        TextField pageCount = new TextField();
-//        pageCount.setPromptText("Page Count");
-//        TextField rating = new TextField();
-//        rating.setPromptText("Rating");
-//
-//        grid.add(new Label("Title:"), 0, 0);
-//        grid.add(title, 1, 0);
-//        grid.add(new Label("Author:"), 0, 1);
-//        grid.add(author, 1, 1);
-//        grid.add(new Label("Finish Date:"), 0, 2);
-//        grid.add(finishDate, 1, 2);
-//        grid.add(new Label("Page Count:"), 0, 3);
-//        grid.add(pageCount, 1, 3);
-//        grid.add(new Label("Rating:"), 0, 4);
-//        grid.add(rating, 1, 4);
-//        dialog.getDialogPane().setContent(grid);
-//
-//
-//        dialog.setResultConverter(dialogButton -> {
-//            if (dialogButton == ButtonType.OK) {
-//                int id = -1;
-//                if (books != null) id = books.getId();
-//                return new Books(title.getText(), author.getText(), finishDate.getText(), pageCount.getText(), rating.getText(), id);
-//            }
-//            return null;
-//        });
-//        if (books != null) {
-//            title.setText(books.getTitle());
-//            author.setText(books.getAuthor());
-//            finishDate.setText(books.getFinishDate());
-//            pageCount.setText(books.getPageCount());
-//            rating.setText(books.getRating());
-//
-//        }
-//        return dialog;
-//    }
-//
-//
-//    }
-//
-//   public void deleteBook(ActionEvent event) {
-//        for (Books books : exampleTable.getSelectionModel().getSelectedItems()) {
-//           BookEntryDAO.delete(books.getId());
-//        }
-//   }
+    public void addBtn(ActionEvent event) throws Exception {
 
+    }
 
-//
+    public void trackBtn(ActionEvent event) throws Exception {
 
+    }
 
+    public void summaryBtn(ActionEvent event) throws Exception {
 
+    }
 
-
-
-
-
-
-
-
-
-//
-//
-//        try {
-////            conn = SqliteConnectionClass.getConnection();
-//            stmt = conn.createStatement();
-//            System.out.println("SUCCESSFUL CONNECTION!\n");
-//            stmt.executeUpdate("INSERT INTO Entries ('Title', 'Author', 'Finish', 'Pages', 'Rating') VALUES ('"+titleField.getText()+"', '"+authorField.getText()+"', '"+finishField.toString()+"','"+countField.getText()+"','"+ratingField.toString()+"'");
-//
-//
-//
-//
-//        } catch (SQLException e) {
-//            Logger.getLogger(SqliteConnectionClass.class.getName()).log(Level.SEVERE, null, e);
-//        }
-
-//        stmt.execute("INSERT INTO Entries ('Title', 'Author', 'Finish Date', 'Page Count', 'Rating') VALUES ("+new Books(titleField.getText(),
-//                authorField.getText(),
-//                finishField.getText(),
-//                countField.getText(),
-////                ratingField.getText())+")");
-
-//
-//        finishDateColumn.setCellValueFactory(new PropertyValueFactory<>("Finish Date"));
-//        pageCountColumn.setCellValueFactory(new PropertyValueFactory<>("Page Count"));
-//        ratingColumn.setCellValueFactory(new PropertyValueFactory<>("Rating"));
-//        idColumn.setCellValueFactory(new PropertyValueFactory<>("ID"));
-//
-//        exampleTable.setItems();
-//
-
-
-//    public void initialize(){
-//        exampleTable.setItems(BookEntryDAO.getBooks());
-//
-//        titleColumn.setCellValueFactory(new PropertyValueFactory<>("Title"));
-//        authorColumn.setCellValueFactory(new PropertyValueFactory<>("Author"));
-//        finishDateColumn.setCellValueFactory(new PropertyValueFactory<>("Finish Date"));
-//        pageCountColumn.setCellValueFactory(new PropertyValueFactory<>("Page Count"));
-//        ratingColumn.setCellValueFactory(new PropertyValueFactory<>("Rating"));
-//        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-
-
+    public void browseBtn(ActionEvent event) throws Exception {
+        try {
+            FXMLLoader loader1 = new FXMLLoader();
+            loader1.setLocation(new URL("file:\\C:\\Users\\haley\\littleLibraryApp\\src\\main\\resources\\com\\example\\littleLibraryApp\\submitBook.fxml"));
+            Parent root1 = (Parent) loader1.load();
+            Stage stage1 = new Stage();
+            stage1.setScene(new Scene(root1));
+            stage1.show();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 
 
 
-//        SqliteConnectionClass connection = new SqliteConnectionClass();
-//        Connection conn = getConnection();
-//        String entry = (("INSERT INTO BookEntry ('Title','Author','Finish Date', 'Page Count', 'Rating') VALUES (" + titleField.getText() + ", " + authorField.getText() + ", " + finishField.getText() + ", " + countField.getText() + ", " + ratingField.getText() + ")"));
-//        Statement statement = conn.createStatement();
-//        statement.executeUpdate(entry);
+
+    }
+
