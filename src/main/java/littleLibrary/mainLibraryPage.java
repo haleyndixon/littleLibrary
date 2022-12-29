c9290dfec889b589049b7f79c5ba76b2b6139819
haley <haleyd1093@gmail.com>
2022-12-28 18:35:31 -0600

    Created a SceneManager and implemented the primary stage in the main file

--- src/main/java/littleLibrary/mainLibraryPage.java
+++ src/main/java/littleLibrary/mainLibraryPage.java
@@ -9,25 +9,28 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+
 public class mainLibraryPage  extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    private static Stage primaryStage;
 
+    public static void main(String[] args) {
+        Application.launch(mainLibraryPage.class);
+    }
     @Override
     public void start(Stage primaryStage) throws IOException {
-
+        mainLibraryPage.primaryStage = primaryStage;
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(new URL("file:\\C:\\Users\\haley\\IdeaProjects\\littleLibrary\\src\\main\\java\\GUI\\Main Page.fxml"));
+        loader.setLocation(new URL("file:\\C:\\Users\\haley\\IdeaProjects\\littleLibrary\\src\\main\\java\\littleLibrary\\Main Page.fxml"));
         AnchorPane mainPane = loader.load();
-
-        Scene mainScene = new Scene(mainPane);
-        primaryStage.setScene(mainScene);
-        primaryStage.setTitle("Little Library");
+        Scene scene = new Scene(mainPane);
+        primaryStage.setScene(scene);
         primaryStage.show();
-    }
 
+    }
+    public static Stage getPrimaryStage() {
+        return primaryStage;
+}
 
 }
 
@@ -58,6 +61,13 @@ public class mainLibraryPage  extends Application {
 
 
 
-
+//    FXMLLoader loader = new FXMLLoader();
+//        loader.setLocation(new URL("file:\\C:\\Users\\haley\\IdeaProjects\\littleLibrary\\src\\main\\java\\GUI\\Main Page.fxml"));
+//        AnchorPane mainPane = loader.load();
+//
+//        Scene mainScene = new Scene(mainPane);
+//        primaryStage.setScene(mainScene);
+//        primaryStage.setTitle("Little Library");
+//        primaryStage.show();
 
 
