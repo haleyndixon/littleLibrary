e045a0b8ee18f3b50ea986b1d88b5f77b1f4f3ec
haley <haleyd1093@gmail.com>
2022-12-31 16:58:55 -0600

    added a feature that allows the user to add a cover photo to the entry

--- src/main/java/littleLibrary/mainLibraryPage.java
+++ src/main/java/littleLibrary/mainLibraryPage.java
@@ -9,65 +9,24 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-
 public class mainLibraryPage  extends Application {
 
-    private static Stage primaryStage;
-
     public static void main(String[] args) {
-        Application.launch(mainLibraryPage.class);
+        launch(args);
     }
+
     @Override
     public void start(Stage primaryStage) throws IOException {
-        mainLibraryPage.primaryStage = primaryStage;
+
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(new URL("file:\\C:\\Users\\haley\\IdeaProjects\\littleLibrary\\src\\main\\java\\littleLibrary\\Main Page.fxml"));
+        loader.setLocation(new URL("file:\\C:\\Users\\haley\\IdeaProjects\\littleLibrary\\src\\main\\java\\GUI\\Main Page.fxml"));
         AnchorPane mainPane = loader.load();
-        Scene scene = new Scene(mainPane);
-        primaryStage.setScene(scene);
+
+        Scene mainScene = new Scene(mainPane);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Little Library");
         primaryStage.show();
-
     }
-    public static Stage getPrimaryStage() {
-        return primaryStage;
-}
+
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    FXMLLoader loader = new FXMLLoader();
-//        loader.setLocation(new URL("file:\\C:\\Users\\haley\\IdeaProjects\\littleLibrary\\src\\main\\java\\GUI\\Main Page.fxml"));
-//        AnchorPane mainPane = loader.load();
-//
-//        Scene mainScene = new Scene(mainPane);
-//        primaryStage.setScene(mainScene);
-//        primaryStage.setTitle("Little Library");
-//        primaryStage.show();
-
-
